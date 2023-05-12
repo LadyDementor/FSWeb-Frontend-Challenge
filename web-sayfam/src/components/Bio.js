fe727920d3ee158ae972cf703aac4c99eb3e66b0
LadyDementor <125830907+LadyDementor@users.noreply.github.com>
2023-05-12 16:45:38 +0300

    KİŞİSEL WEB SAYFAM PROJESİ

--- web-sayfam/src/components/Bio.js
+++ web-sayfam/src/components/Bio.js
@@ -1,42 +1,86 @@
 import React from "react";
+import { Link } from "react-scroll";
 
-export default function Bio() {
+export default function Bio({ language }) {
   return (
-    <div className="hero">
+    <div className="heroBox">
       <div className="heroLeft">
         <div className="isim">
           <div className="line"></div>
-          <p>Elif Koyun</p>
+          <p>Burcu Çalgın</p>
         </div>
-        <h2>
-          Creative thinker
-          <br />
-          Minimalism lover
-        </h2>
-        <p className="isimP">
-          Hi, I’m Almila. I’m a full-stack developer. If you are looking for a
-          Developer who to craft solid and scalable frontend products with great
-          user experiences. Let’s shake hands with me.
-        </p>
-        <div className="buttons">
-          <div className="button-1">
-            <p>Hire me</p>
+        {language === "en" ? (
+          <div>
+            <h2>
+              Creative thinker
+              <br />
+              Minimalism lover
+            </h2>{" "}
+            <br /> <br />
+            <p className="isimP">
+              Hi, I’m Burcu. I’m a full-stack developer. If you are looking for
+              a Developer who to craft solid and scalable frontend products with
+              great user experiences. Let’s shake hands with me.
+            </p>
           </div>
-          <div className="button-2">
-            <img src=".\icons\github.png" />
-            <a href="https://github.com/cosmopoliitans">
-              <p>GitHub</p>
-            </a>
+        ) : (
+          <div>
+            <h2>
+              Yaratıcı Düşünür <br />
+              Minimalizm Aşığı
+            </h2>
+            <p className="isimP">
+              Merhaba, ben Burcu. Ben bir full-stack geliştiricisiyim. Eğer
+              harika kullanıcı tecrübesi olan sağlam frontend ürünler üretebilen
+              bir geliştirici arıyorsanız, birlikte çalışalım.
+            </p>
           </div>
-          <div className="button-2">
-            <img src=".\icons\LinkedIn.png" />
-            <a href="https://www.linkedin.com/in/elif-koyun-3ba895154/">
-              <p>LinkedIn</p>
-            </a>
+        )}
+
+        {language === "en" ? (
+          <div className="buttons">
+            <div className="button-1">
+              <Link to="footer" smooth={true} duration={2000}>
+                <p>Hire me</p>
+              </Link>
+            </div>
+            <div className="button-2">
+              <img src=".\icons\github.png" alt="github" />
+              <a href="https://github.com/LadyDementor">
+                <p>GitHub</p>
+              </a>
+            </div>
+            <div className="button-2">
+              <img src=".\icons\LinkedIn.png" alt="linkedIn" />
+              <a href="https://www.linkedin.com/in/burcu-%C3%A7alg%C4%B1n-52016014a//">
+                <p>LinkedIn</p>
+              </a>
+            </div>
           </div>
-        </div>
+        ) : (
+          <div className="buttons">
+            <div className="button-1">
+              <Link to="footer" smooth={true} duration={2000}>
+                <p>Beni İşe Al</p>
+              </Link>
+            </div>
+            <div className="button-2">
+              <img src=".\icons\github.png" alt="github" />
+              <a href="https://github.com/LadyDementor">
+                <p>GitHub</p>
+              </a>
+            </div>
+            <div className="button-2">
+              <img src=".\icons\LinkedIn.png" alt="linkedIn" />
+              <a href="https://www.linkedin.com/in/burcu-%C3%A7alg%C4%B1n-52016014a/">
+                <p>LinkedIn</p>
+              </a>
+            </div>
+          </div>
+        )}
       </div>
-      <img src=".\icons\image1.png" />
+
+      <img src=".\icons\image1.png" alt="icon" />
     </div>
   );
 }
--- web-sayfam/src/components/Profile.js
+++ web-sayfam/src/components/Profile.js
@@ -1,29 +1,34 @@
 import React from "react";
 
-export default function Profile() {
+export default function Profile({ language }) {
   return (
     <div className="profile">
       <span className="profileLine"></span>
-      <h3>Profile</h3>
+
+      <h3> {language === "en" ? "Profile" : "Profile"}</h3>
       <div className="profileBox1">
         <div className="profileBox">
-          <h4>Profile</h4>
+          <h4>{language === "en" ? "Profile" : "Profil"}</h4>
           <div className="dogumTarihi">
             <div className="solBolum">
-              <p>Doğum Tarihi</p>
-              <p>İkamet Şehri</p>
-              <p>Eğitim Durumu</p>
+              <p>{language === "en" ? "Date of birth" : "Doğum tarihi"}</p>
+              <p>{language === "en" ? "City of residence" : "İkamet şehrim"}</p>
+              <p>
+                {language === "en" ? "Educational status" : "Eğitim bilgilerim"}
+              </p>
               <br />
               <br />
-              <p>Tercih Ettiği Rol</p>
+              <p>
+                {language === "en" ? "Preferred role" : "Tercih ettiğim rol"}
+              </p>
             </div>
             <div className="sagBolum">
-              <p>14.02.1995</p>
+              <p>29.06.1994</p>
               <p>İstanbul</p>
               <p>
-                Erciyes Ünv. ETM
+                Süleyman Demirel Üniversitesi
                 <br />
-                Lisans, 2020
+                {language === "en" ? "Licence" : "Lisans"}, 2017
                 <br />
                 <br />
               </p>
@@ -31,16 +36,35 @@ export default function Profile() {
             </div>
           </div>
         </div>
-        <div className="aboutMe">
-          <h4>About Me</h4>
-          <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam
-            aut, odit laborum aliquam voluptatum nisi mollitia.
-            <br />
-            <br /> Mnima accusamus ratione soluta aperiam sit voluptate? Dicta
-            quod deserunt quam temporibus cumque magnam!
-          </p>
-        </div>
+        {language === "en" ? (
+          <div className="aboutMe">
+            <h4>About Me</h4>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam
+              aut, odit laborum aliquam voluptatum nisi mollitia.
+              <br />
+              <br /> Mnima accusamus ratione soluta aperiam sit voluptate? Dicta
+              quod deserunt quam temporibus cumque magnam!
+            </p>
+          </div>
+        ) : (
+          <div className="aboutMe">
+            <h4>Hakkımda</h4>
+            <p>
+              Süleyman Demirel Üniversitesinde Gıda Mühendisliği bölümünden
+              2017'de mezun oldum.Mevzun olduktan sonra üretim ve satış
+              yöneticiliği alanında toplam 4 sene çalışma tecrübem oldu.
+              <br />
+              <br /> 2022 yılında tekrar üniversite sınavına girerek Ahmet
+              Yesevi Üniversitesini kazandım.Hala okumakta olduğum bölümün
+              2.sınıfındayım.Şu anda aynı zamanda Workintech ile 6 aylık yoğun
+              ve hızlandırılmış full stack web geliştirme kursuna kaydoldum,
+              burada ekip ortamlarında gerçek hayattaki yazılım projelerini
+              öğrenip uyguluyorum. Bu program sayesinde çeşitli teknolojilerde
+              yeterlilik kazandım ve proje geliştirmede aktif olarak yer aldım.
+            </p>
+          </div>
+        )}
       </div>
     </div>
   );
